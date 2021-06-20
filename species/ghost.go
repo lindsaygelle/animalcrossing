d9b0131fd3396e6a45f782a2369724795cac62e5
lindsaygelle <lindsay.gelle@gmail.com>
2021-06-20 14:42:48 +1000

    Added species ghost

---
+++ species/ghost.go
@@ -0,0 +1,19 @@
+package species
+
+import "github.com/lindsaygelle/animalcrossing/animals"
+
+var (
+	// Ghost is the species information for Ghost.
+	Ghost Species = species{
+		class:        na,
+		conservation: na,
+		domain:       na,
+		family:       na,
+		fictional:    fictional,
+		genus:        na,
+		kingdom:      na,
+		name:         animals.Ghost.Name(),
+		order:        na,
+		phylum:       na,
+		species:      na}
+)
