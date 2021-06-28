807239ff5fdb6847df1df32f5fb9dd553d0379b3
lindsaygelle <lindsay.gelle@gmail.com>
2021-06-28 19:33:53 +1000

    Edited exports and testing

--- animals/animals_test.go
+++ animals/animals_test.go
@@ -5,16 +5,24 @@ import (
 	"testing"
 )
 
-func testAnimals(animals []Animal, letter string, t *testing.T) {
+func testAnimals(animals []Animal, character string, t *testing.T) {
 	for i := 0; i < len(animals); i++ {
-		v := animals[i]
-		a := v.Name()
-		b := strings.Title(a)
-		if ok := a == b; !ok {
-			t.Fatalf("%s != %s; i=%d", a, b, i)
-		}
-		if ok := string(b[0]) == letter; !ok {
-			t.Fatalf("%s != %s; i=%d", string(b[i]), letter, i)
-		}
+		testAnimalsCharacter(animals[i], i, character, t)
+		testAnimalsTitle(animals[i], i, t)
+	}
+}
+
+func testAnimalsCharacter(animal Animal, i int, character string, t *testing.T) {
+	var s string = animal.Name()
+	var c string = string(s[0])
+	if ok := c == character; !ok {
+		t.Fatalf("%s != %s; i=%d", c, character, i)
+	}
+}
+
+func testAnimalsTitle(animal Animal, i int, t *testing.T) {
+	var s string = strings.Title(animal.Name())
+	if ok := animal.Name() == s; !ok {
+		t.Fatalf("%s != %s; i=%d", animal.Name(), s, i)
 	}
 }
