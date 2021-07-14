8ad7d47a1d4b2d5cdd03d481dbee539236dbd223
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-14 20:19:07 +1000

    Added to package animals/alpaca

--- animals/alpaca/alpaca.go
+++ animals/alpaca/alpaca.go
@@ -1,6 +1,17 @@
 package alpaca
 
+import "github.com/lindsaygelle/animalcrossing/languages"
+
 type Alpaca struct{}
 
-func (a Alpaca) Id() string    { return "alpaca" }
-func (a Alpaca) Special() bool { return true }
+func (a Alpaca) Id() string {
+	return "alpaca"
+}
+
+func (a Alpaca) Special() bool {
+	return true
+}
+
+func (a Alpaca) Translations() []languages.Translation {
+	return []languages.Translation{}
+}
