aabf29c603d37b1e65d3910a9c801cd017f671e9
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-14 20:24:42 +1000

    Added to package animals/bear

--- animals/bear/bear.go
+++ animals/bear/bear.go
@@ -1,6 +1,30 @@
 package bear
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/bear/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Bear struct{}
 
-func (b Bear) Id() string    { return "bear" }
-func (b Bear) Special() bool { return false }
+func (b Bear) Id() string {
+	return "bear"
+}
+
+func (b Bear) Special() bool {
+	return false
+}
+
+func (b Bear) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
---
+++ animals/bear/translations/zh.go
@@ -0,0 +1,11 @@
+package translations
+
+import "github.com/lindsaygelle/animalcrossing/languages/zh"
+
+type Zh struct {
+	zh.Zh
+}
+
+func (z Zh) Value() string {
+	return "熊"
+}
