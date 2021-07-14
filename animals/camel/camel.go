a7876ced8a73894e4950791729c7b604d85ab6e0
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 00:41:33 +1000

    Added to package animals/camel

--- animals/camel/camel.go
+++ animals/camel/camel.go
@@ -1,6 +1,17 @@
 package camel
 
+import "github.com/lindsaygelle/animalcrossing/languages"
+
 type Camel struct{}
 
-func (c Camel) Id() string    { return "camel" }
-func (c Camel) Special() bool { return true }
+func (c Camel) Id() string {
+	return "camel"
+}
+
+func (c Camel) Special() bool {
+	return true
+}
+
+func (c Camel) Translations() []languages.Translation {
+	return []languages.Translation{}
+}
