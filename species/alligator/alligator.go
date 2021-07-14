ee5a1b96e159944f7468bffbeedee8ed041c273b
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-14 20:20:31 +1000

    Added to package animals/axolotl

--- species/alligator/alligator.go
+++ species/alligator/alligator.go
@@ -1,3 +1,7 @@
 package alligator
 
+import "github.com/lindsaygelle/animalcrossing/animals/alligator"
+
 type Alligator struct{}
+
+func (a Alligator) Id() string { return alligator.Alligator{}.Id() }
