77ebd813bf3ac1a49b6ace89d67b35166e55fab8
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-14 20:16:22 +1000

    Added to package animals/anteater

--- animals/anteater/anteater.go
+++ animals/anteater/anteater.go
@@ -1,6 +1,30 @@
 package anteater
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/anteater/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Anteater struct{}
 
-func (a Anteater) Id() string    { return "anteater" }
-func (a Anteater) Special() bool { return false }
+func (a Anteater) Id() string {
+	return "anteater"
+}
+
+func (a Anteater) Special() bool {
+	return false
+}
+
+func (a Anteater) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
