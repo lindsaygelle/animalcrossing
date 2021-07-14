16ad4c8d190c6a69a297892dfc8f555798e1781a
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 00:37:15 +1000

    Added to package animals/boar

--- animals/boar/boar.go
+++ animals/boar/boar.go
@@ -4,6 +4,14 @@ import "github.com/lindsaygelle/animalcrossing/languages"
 
 type Boar struct{}
 
-func (b Boar) Id() string                            { return "boar" }
-func (b Boar) Special() bool                         { return true }
-func (b Boar) Translations() []languages.Translation { return []languages.Translation{} }
+func (b Boar) Id() string {
+	return "boar"
+}
+
+func (b Boar) Special() bool {
+	return true
+}
+
+func (b Boar) Translations() []languages.Translation {
+	return []languages.Translation{}
+}
