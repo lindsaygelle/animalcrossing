7430ccf003021cc5d0bb6dd1a28840b72c11f489
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 00:36:01 +1000

    Added to package animals/axolot

--- animals/axolotl/axolotl.go
+++ animals/axolotl/axolotl.go
@@ -1,6 +1,9 @@
 package axolotl
 
+import "github.com/lindsaygelle/animalcrossing/languages"
+
 type Axolotl struct{}
 
-func (a Axolotl) Id() string    { return "axolotl" }
-func (a Axolotl) Special() bool { return true }
+func (a Axolotl) Id() string                            { return "axolotl" }
+func (a Axolotl) Special() bool                         { return true }
+func (a Axolotl) Translations() []languages.Translation { return []languages.Translation{} }
