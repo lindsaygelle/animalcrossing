2a14bcac68547ef1017f2a89a0b4938d2dbe35b9
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 00:36:47 +1000

    Added to package animals/axolot

--- animals/axolotl/axolotl.go
+++ animals/axolotl/axolotl.go
@@ -4,6 +4,14 @@ import "github.com/lindsaygelle/animalcrossing/languages"
 
 type Axolotl struct{}
 
-func (a Axolotl) Id() string                            { return "axolotl" }
-func (a Axolotl) Special() bool                         { return true }
-func (a Axolotl) Translations() []languages.Translation { return []languages.Translation{} }
+func (a Axolotl) Id() string {
+	return "axolotl"
+}
+
+func (a Axolotl) Special() bool {
+	return true
+}
+
+func (a Axolotl) Translations() []languages.Translation {
+	return []languages.Translation{}
+}
