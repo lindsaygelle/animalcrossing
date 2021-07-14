cae9a2c4deca3f9f9bf7931a0eac3071dd799a71
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 00:29:01 +1000

    Added to package animals/beaver

--- animals/beaver/beaver.go
+++ animals/beaver/beaver.go
@@ -1,6 +1,17 @@
 package beaver
 
+import "github.com/lindsaygelle/animalcrossing/languages"
+
 type Beaver struct{}
 
-func (b Beaver) Id() string    { return "beaver" }
-func (b Beaver) Special() bool { return true }
+func (b Beaver) Id() string {
+	return "beaver"
+}
+
+func (b Beaver) Special() bool {
+	return true
+}
+
+func (b Beaver) Translations() []languages.Translation {
+	return []languages.Translation{}
+}
