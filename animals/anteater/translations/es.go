dce260be2c7f34f88662fd37a5051db78b98ccdf
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 01:18:21 +1000

    Added to package animals/anteater

--- animals/anteater/translations/es.go
+++ animals/anteater/translations/es.go
@@ -1,9 +1,13 @@
 package translations
 
-import "github.com/lindsaygelle/animalcrossing/languages/es"
+import (
+	"github.com/lindsaygelle/animalcrossing/genders"
+	"github.com/lindsaygelle/animalcrossing/languages/es"
+)
 
 type Es struct {
 	es.Es
+	genders.Male
 }
 
 func (e Es) Value() string {
--- animals/anteater/translations/ru.go
+++ animals/anteater/translations/ru.go
@@ -1,9 +1,13 @@
 package translations
 
-import "github.com/lindsaygelle/animalcrossing/languages/ru"
+import (
+	"github.com/lindsaygelle/animalcrossing/genders"
+	"github.com/lindsaygelle/animalcrossing/languages/ru"
+)
 
 type Ru struct {
 	ru.Ru
+	genders.Male
 }
 
 func (r Ru) Value() string {
