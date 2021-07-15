d1b82092638f8d2a544af897135d3cf4ceb93f12
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 19:54:54 +1000

    Added to package villagers/

--- villagers/villager.go
+++ villagers/villager.go
@@ -1,5 +1,13 @@
 package villagers
 
+import (
+	"github.com/lindsaygelle/animalcrossing/languages"
+	"github.com/lindsaygelle/animalcrossing/species"
+)
+
 type Villager interface {
-	Special()
+	Gender() string
+	Special() bool
+	Species() []species.Species
+	Translations() []languages.Translation
 }
