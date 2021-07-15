2b849c854e38ea1ab2b4cbb6e41b4474a91b7cc1
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 19:44:38 +1000

    Added to package animals/dog

--- animals/dog/dog.go
+++ animals/dog/dog.go
@@ -1,6 +1,34 @@
 package dog
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/dog/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Dog struct{}
 
-func (d Dog) Id() string    { return "dog" }
-func (d Dog) Special() bool { return false }
+func (d Dog) Id() string {
+	return "dog"
+}
+
+func (d Dog) Special() bool {
+	return false
+}
+
+func (d Dog) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.EsFemine{},
+		translations.Es{},
+		translations.FrFemine{},
+		translations.Fr{},
+		translations.ItFemine{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.RuFemine{},
+		translations.Ru{},
+		translations.Zh{}}
+}
---
+++ animals/dog/translations/es_femine.go
@@ -0,0 +1,15 @@
+package translations
+
+import (
+	"github.com/lindsaygelle/animalcrossing/genders"
+	"github.com/lindsaygelle/animalcrossing/languages/es"
+)
+
+type EsFemine struct {
+	es.Es
+	genders.Female
+}
+
+func (e EsFemine) Value() string {
+	return "Perra"
+}
