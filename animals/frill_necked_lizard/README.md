981aa6e3dfeadfe45d3b102ea3719dbac6a69962
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-16 00:21:38 +1000

    Added to package animals/frill_necked_lizard

---
+++ animals/frill_necked_lizard/README.md
@@ -0,0 +1,1 @@
+# Frill-necked Lizard
--- animals/frill_necked_lizard/frill_necked_lizard.go
+++ animals/frill_necked_lizard/frill_necked_lizard.go
@@ -1,6 +1,21 @@
 package frill_necked_lizard
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/frill_necked_lizard/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type FrillNeckedLizard struct{}
 
-func (f FrillNeckedLizard) Id() string    { return "frill_necked_lizard" }
-func (f FrillNeckedLizard) Special() bool { return true }
+func (f FrillNeckedLizard) Id() string {
+	return "frill_necked_lizard"
+}
+
+func (f FrillNeckedLizard) Special() bool {
+	return true
+}
+
+func (f FrillNeckedLizard) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.En{}}
+}
