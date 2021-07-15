fc29d4d3b501ef6f9da09795350dd8fbbdd24593
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-16 00:11:20 +1000

    Added to package animals/elephant

--- animals/elephant/elephant.go
+++ animals/elephant/elephant.go
@@ -1,6 +1,30 @@
 package elephant
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/elephant/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Elephant struct{}
 
-func (e Elephant) Id() string    { return "elephant" }
-func (e Elephant) Special() bool { return false }
+func (e Elephant) Id() string {
+	return "elephant"
+}
+
+func (e Elephant) Special() bool {
+	return false
+}
+
+func (e Elephant) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
