12998bd8db5f2db7a963f2d4d7d60705e2af1eaa
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-16 00:08:18 +1000

    Added to package animals/eagles

--- animals/eagle/eagle.go
+++ animals/eagle/eagle.go
@@ -1,6 +1,30 @@
 package eagle
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/eagle/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Eagle struct{}
 
-func (e Eagle) Id() string    { return "eagle" }
-func (e Eagle) Special() bool { return false }
+func (e Eagle) Id() string {
+	return "eagle"
+}
+
+func (e Eagle) Special() bool {
+	return false
+}
+
+func (e Eagle) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
---
+++ animals/eagle/translations/ru.go
@@ -0,0 +1,15 @@
+package translations
+
+import (
+	"github.com/lindsaygelle/animalcrossing/genders"
+	"github.com/lindsaygelle/animalcrossing/languages/ru"
+)
+
+type Ru struct {
+	ru.Ru
+	genders.Male
+}
+
+func (r Ru) Value() string {
+	return "Орел"
+}
