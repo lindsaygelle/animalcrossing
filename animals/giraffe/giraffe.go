d199f2284adaddf7e730de11c8348497674481b1
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-16 00:32:26 +1000

    Added to package animals/giraffe

--- animals/giraffe/giraffe.go
+++ animals/giraffe/giraffe.go
@@ -1,6 +1,21 @@
 package giraffe
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/giraffe/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Giraffe struct{}
 
-func (g Giraffe) Id() string    { return "giraffe" }
-func (g Giraffe) Special() bool { return true }
+func (g Giraffe) Id() string {
+	return "giraffe"
+}
+
+func (g Giraffe) Special() bool {
+	return true
+}
+
+func (g Giraffe) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.En{}}
+}
