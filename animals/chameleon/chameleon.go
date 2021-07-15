b39ee86596b63cb61b6b29a4712a48847267b2d3
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 18:46:03 +1000

    Added to package animals/chameleon

--- animals/chameleon/chameleon.go
+++ animals/chameleon/chameleon.go
@@ -1,6 +1,17 @@
 package chameleon
 
+import "github.com/lindsaygelle/animalcrossing/languages"
+
 type Chameleon struct{}
 
-func (c Chameleon) Id() string    { return "chameleon" }
-func (c Chameleon) Special() bool { return true }
+func (c Chameleon) Id() string {
+	return "chameleon"
+}
+
+func (c Chameleon) Special() bool {
+	return true
+}
+
+func (c Chameleon) Translations() []languages.Translation {
+	return []languages.Translation{}
+}
