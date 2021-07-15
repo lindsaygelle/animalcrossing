a32dfc76881edfd6368020f9c7ac0aa98369ebd5
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-16 00:16:49 +1000

    Added to package animals/fox

--- animals/fox/fox.go
+++ animals/fox/fox.go
@@ -1,6 +1,21 @@
 package fox
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/fox/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Fox struct{}
 
-func (f Fox) Id() string    { return "fox" }
-func (f Fox) Special() bool { return true }
+func (f Fox) Id() string {
+	return "fox"
+}
+
+func (f Fox) Special() bool {
+	return true
+}
+
+func (f Fox) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.En{}}
+}
