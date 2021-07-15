c1f42e451060940caa16f3f466b7a014f237aada
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-16 00:49:54 +1000

    Added to package animals/gyroid

--- animals/gyroid/gyroid.go
+++ animals/gyroid/gyroid.go
@@ -1,6 +1,21 @@
 package gyroid
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/gyroid/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Gyroid struct{}
 
-func (g Gyroid) Id() string    { return "gyroid" }
-func (g Gyroid) Special() bool { return true }
+func (g Gyroid) Id() string {
+	return "gyroid"
+}
+
+func (g Gyroid) Special() bool {
+	return true
+}
+
+func (g Gyroid) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.En{}}
+}
