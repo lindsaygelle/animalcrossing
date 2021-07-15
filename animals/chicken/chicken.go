3128bc729cdc5c1bcfdf6498f46c79c39a2b54ab
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 19:06:10 +1000

    Added to package animals/chicken

--- animals/chicken/chicken.go
+++ animals/chicken/chicken.go
@@ -1,6 +1,31 @@
 package chicken
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/chicken/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Chicken struct{}
 
-func (c Chicken) Id() string    { return "chicken" }
-func (c Chicken) Special() bool { return false }
+func (c Chicken) Id() string {
+	return "chicken"
+}
+
+func (c Chicken) Special() bool {
+	return false
+}
+
+func (c Chicken) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.DeNeutral{},
+		translations.En{},
+		translations.Es{},
+		translations.FrFemine{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
