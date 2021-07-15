8547cc2fd03f06d3805dfe4bf77410d830d0fdc9
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-15 19:24:57 +1000

    Added to package animals/deer

--- animals/deer/deer.go
+++ animals/deer/deer.go
@@ -1,6 +1,30 @@
 package deer
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/deer/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Deer struct{}
 
-func (d Deer) Id() string    { return "deer" }
-func (d Deer) Special() bool { return false }
+func (d Deer) Id() string {
+	return "deer"
+}
+
+func (d Deer) Special() bool {
+	return false
+}
+
+func (d Deer) Translation() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
---
+++ animals/deer/translations/de.go
@@ -0,0 +1,15 @@
+package translations
+
+import (
+	"github.com/lindsaygelle/animalcrossing/genders"
+	"github.com/lindsaygelle/animalcrossing/languages/de"
+)
+
+type De struct {
+	de.De
+	genders.Male
+}
+
+func (d De) Value() string {
+	return "Hirsch"
+}
