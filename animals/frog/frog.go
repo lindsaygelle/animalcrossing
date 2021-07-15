f6e53ccd61637c613f30d1a7f0a19ce287e25ed6
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-16 00:25:39 +1000

    Added to package animals/frog

--- animals/frog/frog.go
+++ animals/frog/frog.go
@@ -1,6 +1,30 @@
 package frog
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/frog/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Frog struct{}
 
-func (f Frog) Id() string    { return "frog" }
-func (f Frog) Special() bool { return false }
+func (f Frog) Id() string {
+	return "frog"
+}
+
+func (f Frog) Special() bool {
+	return false
+}
+
+func (f Frog) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
---
+++ animals/frog/translations/nl.go
@@ -0,0 +1,15 @@
+package translations
+
+import (
+	"github.com/lindsaygelle/animalcrossing/genders"
+	"github.com/lindsaygelle/animalcrossing/languages/nl"
+)
+
+type Nl struct {
+	nl.Nl
+	genders.None
+}
+
+func (n Nl) Value() string {
+	return "Kikker"
+}
