2cd9645b13e650a01fb4753ff332b739fc9f12cf
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-18 23:52:33 +1000

    Added to package animals/hedgehog

--- animals/hedgehog/hedgehog.go
+++ animals/hedgehog/hedgehog.go
@@ -1,6 +1,21 @@
 package hedgehog
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/hedgehog/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Hedgehog struct{}
 
-func (h Hedgehog) Id() string    { return "hedgehog" }
-func (h Hedgehog) Special() bool { return true }
+func (h Hedgehog) Id() string {
+	return "hedgehog"
+}
+
+func (h Hedgehog) Special() bool {
+	return true
+}
+
+func (h Hedgehog) Value() []languages.Translation {
+	return []languages.Translation{
+		translations.En{}}
+}
