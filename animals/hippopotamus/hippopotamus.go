83702b3429e4f9da1c6e84b6bae899baceaa41df
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-18 23:57:07 +1000

    Added to package animals/hippopotamus

--- animals/hippopotamus/hippopotamus.go
+++ animals/hippopotamus/hippopotamus.go
@@ -1,6 +1,30 @@
 package hippopotamus
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/hippopotamus/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Hippopotamus struct{}
 
-func (h Hippopotamus) Id() string    { return "hippopotamus" }
-func (h Hippopotamus) Special() bool { return false }
+func (h Hippopotamus) Id() string {
+	return "hippopotamus"
+}
+
+func (h Hippopotamus) Special() bool {
+	return false
+}
+
+func (h Hippopotamus) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
