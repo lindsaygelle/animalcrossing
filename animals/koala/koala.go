33a619a90bba4f0156281b5101e1935075285203
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-19 23:28:29 +1000

    Added to package animals/koala

--- animals/koala/koala.go
+++ animals/koala/koala.go
@@ -1,6 +1,30 @@
 package koala
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/koala/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Koala struct{}
 
-func (k Koala) Id() string    { return "koala" }
-func (k Koala) Special() bool { return false }
+func (k Koala) Id() string {
+	return "koala"
+}
+
+func (k Koala) Special() bool {
+	return false
+}
+
+func (k Koala) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
