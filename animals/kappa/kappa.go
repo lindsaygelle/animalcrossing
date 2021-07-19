9ff083827f414b871e1535054ead593a7ba48efb
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-19 23:24:11 +1000

    Added to package animals/kappa

--- animals/kappa/kappa.go
+++ animals/kappa/kappa.go
@@ -1,6 +1,21 @@
 package kappa
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/kappa/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Kappa struct{}
 
-func (k Kappa) Id() string    { return "kappa" }
-func (k Kappa) Special() bool { return true }
+func (k Kappa) Id() string {
+	return "kappa"
+}
+
+func (k Kappa) Special() bool {
+	return true
+}
+
+func (k Kappa) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.En{}}
+}
