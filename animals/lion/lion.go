3029982dfb2042e79328e43ca035ac8ec2ab662b
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-19 23:35:42 +1000

    Added to package animals/lion

--- animals/lion/lion.go
+++ animals/lion/lion.go
@@ -1,6 +1,30 @@
 package lion
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/lion/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Lion struct{}
 
-func (l Lion) Id() string    { return "lion" }
-func (l Lion) Special() bool { return false }
+func (l Lion) Id() string {
+	return "lion"
+}
+
+func (l Lion) Special() bool {
+	return false
+}
+
+func (l Lion) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
---
+++ animals/lion/translations/zh.go
@@ -0,0 +1,15 @@
+package translations
+
+import (
+	"github.com/lindsaygelle/animalcrossing/genders"
+	"github.com/lindsaygelle/animalcrossing/languages/zh"
+)
+
+type Zh struct {
+	zh.Zh
+	genders.None
+}
+
+func (z Zh) Value() string {
+	return "狮子"
+}
