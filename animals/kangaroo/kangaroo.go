df238387744d84b4edb771d5c8e1ad246384e57b
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-19 20:10:00 +1000

    Added to package animals/kangaroo

--- animals/kangaroo/kangaroo.go
+++ animals/kangaroo/kangaroo.go
@@ -1,6 +1,30 @@
 package kangaroo
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/kangaroo/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Kangaroo struct{}
 
-func (k Kangaroo) Id() string    { return "kangaroo" }
-func (k Kangaroo) Special() bool { return false }
+func (k Kangaroo) Id() string {
+	return "kangaroo"
+}
+
+func (k Kangaroo) Special() bool {
+	return false
+}
+
+func (k Kangaroo) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
---
+++ animals/kangaroo/translations/ko.go
@@ -0,0 +1,15 @@
+package translations
+
+import (
+	"github.com/lindsaygelle/animalcrossing/genders"
+	"github.com/lindsaygelle/animalcrossing/languages/ko"
+)
+
+type Ko struct {
+	ko.Ko
+	genders.Male
+}
+
+func (k Ko) Value() string {
+	return "캥거루"
+}
