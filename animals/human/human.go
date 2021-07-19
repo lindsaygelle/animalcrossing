a908f2f923a5fb78635fff65200f0a56a5312217
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-19 20:00:15 +1000

    Added to package animals/human

--- animals/human/human.go
+++ animals/human/human.go
@@ -1,6 +1,21 @@
 package human
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/human/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Human struct{}
 
-func (h Human) Id() string    { return "human" }
-func (h Human) Special() bool { return true }
+func (h Human) Id() string {
+	return "human"
+}
+
+func (h Human) Special() bool {
+	return true
+}
+
+func (h Human) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.En{}}
+}
