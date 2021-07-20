d66c581bd5843ad9ca627cee8594ca481afff8d4
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-20 23:50:51 +1000

    Added to package animals/monkey

--- animals/monkey/monkey.go
+++ animals/monkey/monkey.go
@@ -1,6 +1,32 @@
 package monkey
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/monkey/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Monkey struct{}
 
-func (m Monkey) Id() string    { return "monkey" }
-func (m Monkey) Special() bool { return false }
+func (m Monkey) Id() string {
+	return "monkey"
+}
+
+func (m Monkey) Special() bool {
+	return false
+}
+
+func (m Monkey) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.EsFemine{},
+		translations.Es{},
+		translations.FrFemine{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
