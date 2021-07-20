ba063c30f24cfa5274e0bab70b1ad73a128c8aeb
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-20 23:45:14 +1000

    Added to package animals/mole

--- animals/mole/mole.go
+++ animals/mole/mole.go
@@ -1,6 +1,21 @@
 package mole
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/mole/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Mole struct{}
 
-func (m Mole) Id() string    { return "mole" }
-func (m Mole) Special() bool { return true }
+func (m Mole) Id() string {
+	return "mole"
+}
+
+func (m Mole) Special() bool {
+	return true
+}
+
+func (m Mole) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.En{}}
+}
