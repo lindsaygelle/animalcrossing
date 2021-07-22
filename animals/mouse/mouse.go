9216e7a1ee7968c227c1b29becd4861b126888f8
lindsaygelle <lindsay.gelle@gmail.com>
2021-07-22 19:17:02 +1000

    Added to package animals/mouse

--- animals/mouse/mouse.go
+++ animals/mouse/mouse.go
@@ -1,6 +1,31 @@
 package mouse
 
+import (
+	"github.com/lindsaygelle/animalcrossing/animals/mouse/translations"
+	"github.com/lindsaygelle/animalcrossing/languages"
+)
+
 type Mouse struct{}
 
-func (m Mouse) Id() string    { return "mouse" }
-func (m Mouse) Special() bool { return false }
+func (m Mouse) Id() string {
+	return "mouse"
+}
+
+func (m Mouse) Special() bool {
+	return false
+}
+
+func (m Mouse) Translations() []languages.Translation {
+	return []languages.Translation{
+		translations.De{},
+		translations.En{},
+		translations.EsFemine{},
+		translations.Es{},
+		translations.Fr{},
+		translations.It{},
+		translations.Jp{},
+		translations.Ko{},
+		translations.Nl{},
+		translations.Ru{},
+		translations.Zh{}}
+}
