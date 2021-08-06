98f2426caa00fd72f109fd6a28531693fddeb477
lindsaygelle <lindsay.gelle@gmail.com>
2021-08-06 17:11:58 +1000

    Added to villager names

--- villager/alligator/alfonso/name.go
+++ villager/alligator/alfonso/name.go
@@ -3,28 +3,34 @@ package alfonso
 import "golang.org/x/text/language"
 
 const (
-	nameAmericanEnglish    string = "Alfonso"
-	nameFrench             string = "Alphonse"
-	nameGerman             string = "Markus"
-	nameItalian            string = "Alfonso"
-	nameJapanese           string = "アルベルト"
-	nameKorean             string = "알베르트"
-	nameRussian            string = "Альфонсо"
-	nameSpanish            string = "Kaimán"
-	nameSimplifiedChinese  string = "阿泥"
-	nameTraditionalChinese string = "阿泥"
+	nameAmericanEnglish      string = "Alfonso"
+	nameCanadianFrench       string = "Alphonse"
+	nameDutch                string = "Alfonso"
+	nameFrench               string = "Alphonse"
+	nameGerman               string = "Markus"
+	nameItalian              string = "Alfonso"
+	nameJapanese             string = "アルベルト"
+	nameLatinAmericanSpanish string = "Kaimán"
+	nameKorean               string = "알베르트"
+	nameRussian              string = "Альфонсо"
+	nameSpanish              string = "Kaimán"
+	nameSimplifiedChinese    string = "阿泥"
+	nameTraditionalChinese   string = "阿泥"
 )
 
 var (
 	name = map[language.Tag]string{
-		language.AmericanEnglish:    nameAmericanEnglish,
-		language.French:             nameFrench,
-		language.German:             nameGerman,
-		language.Italian:            nameItalian,
-		language.Japanese:           nameJapanese,
-		language.Korean:             nameKorean,
-		language.Russian:            nameRussian,
-		language.Spanish:            nameSpanish,
-		language.SimplifiedChinese:  nameSimplifiedChinese,
-		language.TraditionalChinese: nameTraditionalChinese}
+		language.AmericanEnglish:      nameAmericanEnglish,
+		language.CanadianFrench:       nameCanadianFrench,
+		language.Dutch:                nameDutch,
+		language.French:               nameFrench,
+		language.German:               nameGerman,
+		language.Italian:              nameItalian,
+		language.Japanese:             nameJapanese,
+		language.Korean:               nameKorean,
+		language.LatinAmericanSpanish: nameLatinAmericanSpanish,
+		language.Russian:              nameRussian,
+		language.Spanish:              nameSpanish,
+		language.SimplifiedChinese:    nameSimplifiedChinese,
+		language.TraditionalChinese:   nameTraditionalChinese}
 )
